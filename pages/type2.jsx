c2589a75c72f2553e9d717ab87fc95d7ff9986d6
Yousef Aburadi <yousefaburadi@yahoo.com>
2022-09-29 20:59:33 +0300

    first commit

---
+++ pages/type2.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+/**
+ *
+ */
+const type2 = () => {
+  return (
+    <div>
+      <iframe
+        style={{
+          height: "100vh",
+          width: "100vw",
+        }}
+        src="https://wordwall.net/embed/e851a807250444719428b129fd01ee14?themeId=46&templateId=46&fontStackId=0"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+};
+export default type2;
