3e063687b02c5755d2ab16cf79ad3a35ddff7af8
Yousef Aburadi <yousefaburadi@yahoo.com>
2022-09-29 21:17:23 +0300

    fix the link problem

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from "../styles/Home.module.css";
+import Link from "next/link";
 export default function home() {
   return (
     <div className={style.container}>
-      <a href="/type2">انواع الخطر 2</a>
-      <a href="/type">انواع الخطر 1</a>
+      <Link href="/type2">انواع الخطر 2</Link>
+      <Link href="/type">انواع الخطر 1</Link>
     </div>
   );
 }
