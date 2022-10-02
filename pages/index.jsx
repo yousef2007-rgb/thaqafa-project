e97796c77c3c7b983139484c2a55c8f40cf64523
Yousef Aburadi <yousefaburadi@yahoo.com>
2022-10-03 00:29:09 +0300

    add powerpoint link

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,12 @@ export default function home() {
     <div className={style.container}>
       <Link href="/type2">انواع الخطر 2</Link>
       <Link href="/type">انواع الخطر 1</Link>
+      <a
+        href="https://docs.google.com/presentation/d/1tkk-xUu_m-gCiZc5g94q8mFw8f57JmBZ/edit?usp=drivesdk&ouid=108923277493221566731&rtpof=true&sd=true"
+        target="blank"
+      >
+        PowerPoint
+      </a>
     </div>
   );
 }
